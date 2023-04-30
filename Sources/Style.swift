e42933240ef8ee095adac6e1125f53e20aaa7b5c
Andriy Gordiyenko <andgordio@gmail.com>
2023-04-30 00:49:43 -0700

    Alternative names

--- Sources/Style.swift
+++ Sources/Style.swift
@@ -51,17 +51,40 @@ public struct StyleModifier: ViewModifier {
         case .fg(let c): content.foregroundColor(c)
         case .font(let s): content.font(.system(size: s))
         case .rounded(let r): content.cornerRadius(r)
+            
+        case .width(let i): content.frame(width: i)
+        case .widthInfinity: content.frame(maxWidth: .infinity)
+        case .height(let i): content.frame(height: i)
+        case .heightInfinity: content.frame(maxHeight: .infinity)
+        case .padding(let i): content.padding(i)
+        case .paddingHorizontal(let i): content.padding(.horizontal, i)
+        case .paddingVertical(let i): content.padding(.vertical, i)
+        case .paddingTop(let i): content.padding(.top, i)
+        case .paddingBottom(let i): content.padding(.bottom, i)
+        case .paddingLeading(let i): content.padding(.leading, i)
+        case .paddingTrailing(let i): content.padding(.trailing, i)
+        case .backgroundColor(let c): content.background(c)
+        case .foregroundColor(let c): content.foregroundColor(c)
+        case .fontSize(let s): content.font(.system(size: s))
+        case .cornerRadius(let r): content.cornerRadius(r)
         }
     }
 }
 
 @available(iOS 13.0.0, *)
 public enum StyleParam {
+    
+    /*
+     OPTION A
+     Short names
+     */
+    
     // Frame
     case w(CGFloat)
     case wFull
     case h(CGFloat)
     case hFull
+    
     // Paddings
     case p(CGFloat = 20)
     case ph(CGFloat = 20)
@@ -70,12 +93,44 @@ public enum StyleParam {
     case pb(CGFloat = 20)
     case pl(CGFloat = 20)
     case ptr(CGFloat = 20)
+    
     // Colors
     case bg(Color = .blue)
     case fg(Color = .red)
+    
     // Text
     case font(CGFloat = 15)
+    
     // Other
     case rounded(CGFloat = 10)
-    // case stroke(CGFloat)
+    
+    /*
+     OPTION B
+     Aligned with SwiftUI names
+     */
+    
+    // Frame
+    case width(CGFloat)
+    case widthInfinity
+    case height(CGFloat)
+    case heightInfinity
+    
+    // Paddings
+    case padding(CGFloat = 20)
+    case paddingHorizontal(CGFloat = 20)
+    case paddingVertical(CGFloat = 20)
+    case paddingTop(CGFloat = 20)
+    case paddingBottom(CGFloat = 20)
+    case paddingLeading(CGFloat = 20)
+    case paddingTrailing(CGFloat = 20)
+    
+    // Colors
+    case backgroundColor(Color = .blue)
+    case foregroundColor(Color = .red)
+    
+    // Text
+    case fontSize(CGFloat = 15)
+    
+    // Other
+    case cornerRadius(CGFloat = 10)
 }
